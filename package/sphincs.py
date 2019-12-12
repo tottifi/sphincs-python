09d972b86267f2093460ed715e69806388f0e2d2
Antoine <antoine.carlier@isen.yncrea.fr>
2019-12-12 13:52:07 +0100

    Bug Fix: Int too ling for to_bytes

--- package/sphincs.py
+++ package/sphincs.py
@@ -25,7 +25,7 @@ def hash(seed, adrs: ADRS, value, digest_size):
 
 def prf(secret_seed, adrs, digest_size):
     random.seed(int.from_bytes(secret_seed + adrs.to_bin(), "big"))
-    return random.randint(0, 256 ** digest_size).to_bytes(digest_size, byteorder='big')
+    return random.randint(0, 256 ** digest_size - 1).to_bytes(digest_size, byteorder='big')
 
 
 def hash_msg(r, public_seed, public_root, value, digest_size):
@@ -56,7 +56,7 @@ def hash_msg(r, public_seed, public_root, value, digest_size):
 
 def prf_msg(secret_seed, opt, m, digest_size):
     random.seed(int.from_bytes(secret_seed + opt + hash_msg(b'0', b'0', b'0', m, digest_size * 2), "big"))
-    return random.randint(0, 256 ** digest_size).to_bytes(digest_size, byteorder='big')
+    return random.randint(0, 256 ** digest_size - 1).to_bytes(digest_size, byteorder='big')
 
 
 # Input: len_X-byte string X, int w, output length out_len
@@ -329,7 +329,7 @@ class Sphincs():
             csum += self._w - 1 - msg[i]
 
         csum = csum << (8 - ((self._len_2 * math.floor(math.log(self._w, 2))) % 8))
-        csumb = csum.to_bytes(math.ceil((self._len_2 * math.floor(math.log(self._w, 2))) / 8), byteorder='big')
+        csumb = csum.to_bytes(math.ceil((self._len_2 * math.floor(math.log(self._w, 2))) / 8) + 1, byteorder='big')
         csumw = base_w(csumb, self._w, self._len_2)
         msg += csumw
 
@@ -352,7 +352,7 @@ class Sphincs():
             csum += self._w - 1 - msg[i]
 
         csum = csum << (8 - ((self._len_2 * math.floor(math.log(self._w, 2))) % 8))
-        csumb = csum.to_bytes(math.ceil((self._len_2 * math.floor(math.log(self._w, 2))) / 8), byteorder='big')
+        csumb = csum.to_bytes(math.ceil((self._len_2 * math.floor(math.log(self._w, 2))) / 8) + 1, byteorder='big')
         csumw = base_w(csumb, self._w, self._len_2)
         msg += csumw
 
